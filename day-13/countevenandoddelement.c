bf4a6e843ea9404725d6283b8035f96866c0757b
deewakar9260 <deewakarsahani9@gmail.com>
2026-06-13 18:54:48 +0530

    Add program to count even and odd elements in array

---
+++ day-13/countevenandoddelement.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    int size;
+    int even_count = 0;
+    int odd_count = 0;
+
+    printf("Enter the size of the array: ");
+    if (scanf("%d", &size) != 1 || size <= 0) {
+        printf("Invalid array size.\n");
+        return 1;
+    }
+
+    int arr[size];
+
+    printf("Enter %d elements:\n", size);
+    for (int i = 0; i < size; i++) {
+        if (scanf("%d", &arr[i]) != 1) {
+            printf("Invalid input element.\n");
+            return 1;
+        }
+    }
+
+    for (int i = 0; i < size; i++) {
+        if (arr[i] % 2 == 0) {
+            even_count++;
+        } else {
+            odd_count++;
+        }
+    }
+
+    printf("Total Even elements: %d\n", even_count);
+    printf("Total Odd elements: %d\n", odd_count);
+
+    return 0;
+}
