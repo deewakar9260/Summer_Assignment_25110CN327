b09a3e249fd9fe77be7ac480634f166f151dd088
deewakar9260 <deewakarsahani9@gmail.com>
2026-06-15 17:59:41 +0530

    program to move zeros to the end of array

---
+++ day-15/movezerostoend.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+void moveZerosToEnd(int arr[], int n) {
+    int count = 0;
+    
+    for (int i = 0; i < n; i++) {
+        if (arr[i] != 0) {
+            arr[count++] = arr[i];
+        }
+    }
+    
+    while (count < n) {
+        arr[count++] = 0;
+    }
+}
+
+int main() {
+    int arr[] = {1, 2, 0, 4, 3, 0, 5, 0};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    
+    moveZerosToEnd(arr, n);
+    
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    
+    return 0;
+}
