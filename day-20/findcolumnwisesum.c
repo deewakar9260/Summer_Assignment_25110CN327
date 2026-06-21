ab55b7b4390a2f3aae882a8d0a528da24be35121
deewakar9260 <deewakarsahani9@gmail.com>
2026-06-21 09:16:46 +0530

    Add column-wise sum calculation for a matrix

---
+++ day-20/findcolumnwisesum.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int rows, cols;
+    
+    printf("Enter the number of rows and columns: ");
+    scanf("%d %d", &rows, &cols);
+    
+    int matrix[rows][cols];
+    
+    printf("Enter the elements of the matrix:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+    
+    for (int j = 0; j < cols; j++) {
+        int col_sum = 0;
+        for (int i = 0; i < rows; i++) {
+            col_sum += matrix[i][j];
+        }
+        printf("Sum of column %d = %d\n", j + 1, col_sum);
+    }
+    
+    return 0;
+}
