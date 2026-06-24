5de9857020c86554f6eae80a578cea9bfb0875cd
deewakar9260 <deewakarsahani9@gmail.com>
2026-06-24 11:17:35 +0530

    Add program to convert between lowercase and uppercase

---
+++ day-21/convertlowercaseanduppercase.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i = 0;
+
+    printf("Enter a string: ");
+    if (fgets(str, sizeof(str), stdin) == NULL) {
+        return 1;
+    }
+
+    while (str[i] != '\0') {
+        if (str[i] >= 'a' && str[i] <= 'z') {
+            str[i] = str[i] - 32;
+        } else if (str[i] >= 'A' && str[i] <= 'Z') {
+            str[i] = str[i] + 32;
+        }
+        i++;
+    }
+
+    printf("Converted string: %s", str);
+
+    return 0;
+}
