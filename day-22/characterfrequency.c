0180371a62b479ef762d512205565a339d8fccb1
deewakar9260 <deewakarsahani9@gmail.com>
2026-06-25 20:38:46 +0530

    Add character frequency counter

---
+++ day-22/characterfrequency.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    char str[1000];
+    int freq[256] = {0};
+    int i = 0;
+
+    printf("Enter a string: ");
+    if (fgets(str, sizeof(str), stdin) == NULL) {
+        return 1;
+    }
+
+    while (str[i] != '\0') {
+        if (str[i] != '\n') {
+            freq[(unsigned char)str[i]]++;
+        }
+        i++;
+    }
+
+    printf("\nCharacter Frequencies:\n");
+    for (i = 0; i < 256; i++) {
+        if (freq[i] > 0) {
+            printf("'%c' : %d\n", i, freq[i]);
+        }
+    }
+
+    return 0;
+}
